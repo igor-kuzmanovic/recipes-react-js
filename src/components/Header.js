a481621e6c81b49400159623fdace7069f5e782c
ikuzmanovic <igor.kuzmanovic.996@gmail.com>
2019-03-06 13:20:02 +0100

    Header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,29 +5,31 @@ import { LinkContainer } from 'react-router-bootstrap';
 const Header = () => {
     return (
         <Navbar bg="light" expand="md">
-            <LinkContainer to="/">
-                <Navbar.Brand>RecipesApp</Navbar.Brand>
-            </LinkContainer>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
+            <div className="container">
                 <LinkContainer to="/">
-                    <Nav.Link>Home</Nav.Link>
+                    <Navbar.Brand>RecipesApp</Navbar.Brand>
                 </LinkContainer>
-                <LinkContainer to="/recipes">
-                    <Nav.Link>Recipes</Nav.Link>
-                </LinkContainer>
-                <LinkContainer to="/ingredients">
-                    <Nav.Link>Ingredients</Nav.Link>
-                </LinkContainer>
-                <LinkContainer to="/categories">
-                    <Nav.Link>Categories</Nav.Link>
-                </LinkContainer>
-                <LinkContainer to="/tags">
-                    <Nav.Link>Tags</Nav.Link>
-                </LinkContainer>
-            </Nav>
-            </Navbar.Collapse>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto">
+                        <LinkContainer to="/">
+                            <Nav.Link>Home</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/recipes">
+                            <Nav.Link>Recipes</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/ingredients">
+                            <Nav.Link>Ingredients</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/categories">
+                            <Nav.Link>Categories</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/tags">
+                            <Nav.Link>Tags</Nav.Link>
+                        </LinkContainer>
+                    </Nav>
+                </Navbar.Collapse>
+            </div>
         </Navbar>
     )
 };
